c7480fcee95689191a8c47c6df43b621038a9144
algovera <stanley.chung@tbcasoft.com>
2020-12-25 11:47:46 +0800

    1st part complete

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,14 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo,
+    editTodo,
+    deleteTodo
 } = require('./APIs/todos')
 
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.put('/todo/:todoId', editTodo);
+app.delete('/todo/:todoId', deleteTodo);
 exports.api = functions.https.onRequest(app);
-
-
-
